5f8f41462b5f44306a25c4954e0381df9edd16b4
Wisnu Sanjaya <wsanjaya69@gmail.com>
2025-07-08 15:42:47 +0700

    tahap 1.1

---
+++ tahap1/trade_history.py
@@ -0,0 +1,33 @@
+trade = []
+
+while True:
+    print("1. Tambah Trade")
+    print("2. Lihat Semua Trade")
+    print("0. Keluar")
+    pilihan = int(input("Masukan Pilihan : "))
+    if pilihan == 0:
+        break
+    elif pilihan == 1:
+        tanggal = input("Masukan Tanggal ( Contoh: 17-08-1945): ")
+        pair = str(input("Pair (Misal: GBPUSD) : "))
+        posisi = str(input("Posisi (Buy/ Sell) : "))
+        lot = float(input(" Lot (Misal: 0.5) : "))
+        entry = float(input("Masukan Harga Entry : "))
+        sl = float(input("Masukan harga SL : "))
+        tp = float(input("Masukan harga TP : "))
+        hasil = float(input("Masukan Profit/Loss dalam usd : "))
+        catatan = str(input("Catatan (Strategi, Alasan Entry, alasan psikologi atau yang lainnya) : "))
+        trade_baru = {
+            "tanggal": tanggal,
+            "Pair": pair,
+            "posisi": posisi,
+            "lot": lot,
+            "entry": entry,
+            "SL": sl,
+            "TP": tp,
+            "hasil": hasil,
+            "catatan": catatan
+        }
+        trade.append(trade_baru)
+    elif pilihan == 2:
+        print(trade)
