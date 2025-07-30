06eb1a0eb0b3d634c24373bef112479f48c46e05
kezenu <wsanjaya69@gmail.com>
2025-07-30 21:41:13 +0700

    pandas 3 oke

--- latihan/pandas3.py
+++ latihan/pandas3.py
@@ -1,5 +1,27 @@
 import pandas as pd
 
+"""
+1 Jika:
+close > ma3
+dan rsi < 30
+dan volume > 1500
+dan close > open
+Maka: entry_buy
+2 Jika:
+close < ma3
+dan rsi > 70
+dan volume > 1500
+dan close < open
+Maka: entry_sell
+3 Jika:
+RSI antara 45–55
+dan volume < 1000
+Maka: no_entry_zona_netral
+Selain itu: wait
+
+"""
+
+
 data = {
     "open":   [100, 105, 110, 108, 115, 120, 117, 122, 119, 121],
     "high":   [103, 107, 113, 111, 118, 124, 119, 125, 121, 123],
@@ -11,3 +33,18 @@ data = {
 }
 
 df = pd.DataFrame(data)
+
+class Trade:
+    def buy(self, row):
+        if row['close'] > row['ma3'] and row['rsi'] < 30 and row['volume'] > 1500 and row['close'] > row['open']:
+            return 'buy'
+        elif row['close'] < row['ma3'] and row['rsi'] > 70 and row['volume'] > 1500 and row['close'] < row['open']:
+            return 'sell'
+        elif 45 <= row['rsi'] <= 55 and row['volume'] < 1000:
+            return 'No Entry Zona Netral'
+        else:
+            return 'wait'
+        
+trade = Trade()
+df['analisa'] = df.apply(trade.buy, axis=1)
+print(df)
